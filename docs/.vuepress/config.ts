ba2617abc1e4f5bcdb2d914f3a718337ef30d872
Burner <burner@burner.gay>
2022-07-04 15:33:05 -0500

    Minor tweaks

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,8 @@
+import { defineUserConfig } from "vuepress"
+
+export default defineUserConfig({
+  lang: "en-US",
+  title: "Sticker Bot Docs",
+  description: "Discord sticker bot docs",
+  base: "/sticker-bot/",
+})
